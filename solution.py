b8e4474840e87878b1b33de8b4f566c20fb173aa
Shawn Oakley <shawnoakley@gmail.com>
2014-03-10 06:27:36 -0400

    Implements basic functionality.

--- solution.py
+++ solution.py
@@ -6,6 +6,9 @@ import urlunshort
 import httplib
 import urlparse
 
+# NOTE: 'uri' is used throughout to refer to 'urls' as a way to avoid inadvertant conflicts 
+# with method names in any of the python modules
+
 # Sets up tweepy for API calls to Twitter
 # Saving the consumer secret and consumer key in this manner isn't ideal.
 # However, as the API is a dummy app created for the purposes of this exercise,
@@ -24,58 +27,39 @@ api = tweepy.API(auth)
 
 # Grabs test data from delve API and extracts relevant search/comparison parameters
 
-data = json.load(urllib2.urlopen('http://delvenews.com/api/matador/'))
+data = json.load(urllib2.urlopen('http://delvenews.com/api/matador/?email=shawnoakley@gmail.com'))
 handle_list = data['twitter_handles']
 start_date = data['begin_date']
 end_date = data['end_date']
 
 # Establishes minimum domain match count to label a comparison between handles as 'true'
 
-min_count = data['match_criteria']
+min_count = 1
 
 # Generates regex used to check for url in tweet text.
 # Note the use of the capture group, which targets the top-level domain.
 # By including this in the first regex used, and then calling <regex_return>.group(1)
-# I can avoid using the regex more than once per tweet.
+# I could (ideally) avoid using the regex more than once per tweet.
 
-trunc_regex = re.compile('http[s]?:\/\/(.+\.){0,3}([A-Za-z0-9]*)\.(co|\w):?[0-9]*\/?\w*\d*')
-full_regex = re.compile('http[s]?:\/\/(.+\.){0,3}([A-Za-z0-9]*)\.(com|net|co.uk|gov|ly|in|\w):?[0-9]*\/?\w*\d*')
+http_regex = re.compile('http[s]?:\/\/([-A-Za-z0-9+&@#%?=~_()|!:,.;]*\.?[-A-Za-z0-9+&@#%?=~_()|!:,.;]*)\/?:?\w*\d*')
 
-# test_search = api.search(handle_list[2], since=start_date, until=end_date)
+# Identifies whether a given tweet contains either a shortened url or a long url
 
-# NEXT STEP:  regex http identifier in tweet text 
-# http://blog.codinghorror.com/the-problem-with-urls/
-# http://www.regexguru.com/2008/11/detecting-urls-in-a-block-of-text/
 def uri_tweet_identifier(regex, tweet_text):
-	uri_search = re.search(full_regex, tweet_text)
-	print 'tweet text'
-	print tweet_text
-	print 'URI search 1'
-	print uri_search.group()
-	if uri_search is None:
-		uri_search = re.search(trunc_regex, tweet_text)
-		print 'URI search 2'
-		print uri_search.group()
+	uri_search = re.search(regex, tweet_text)
 
 	# The first 'if' test deals with urls that were truncated in the tweet's text
 	# and are therefore un-openable or where no url is in the tweet's text
 
-
-	if uri_search is None or len(uri_search.group()) < 13:
+	if uri_search is None or len(uri_search.group()) < 12:
 		return None
 	else:
-		# Domain extraction call should go here.  Return extracted domain.
-		# Expander is nested in extractor
 		return extract_domain(uri_search)
 
-# Checking for short URLs
-# https://stackoverflow.com/questions/9557116/regex-php-code-to-check-if-a-url-is-a-short-url
-
-# https://stackoverflow.com/questions/4201062/how-can-i-unshorten-a-url-using-python
+# Expands shortened url.  Does so by opening the actual shortened uri and seeing what
+# the destination uri is  
 
 def unshorten_uri(uri):
-	# print 'URI: '
-	# print uri
 	parsed = urlparse.urlparse(uri)
 	h = httplib.HTTPConnection(parsed.netloc)
 	h.request('HEAD', parsed.path)
@@ -89,35 +73,23 @@ def unshorten_uri(uri):
 	else:
 		return uri
 
+# Extracts a standard domain name from the url, irrespective of longer 
+# pathnames or parameters  
+
 def extract_domain(uri_text):
-	print 'URI Text'
-	print uri_text.group()
+	# print 'URI Text'
+	# print uri_text.group()
 	expanded_uri_text = unshorten_uri(uri_text.group())
-	expanded_uri_search = re.search(full_regex, expanded_uri_text)
-	print 'Full'
-	print expanded_uri_search.group()
-	print 'Domain'
+	expanded_uri_search = re.search(http_regex, expanded_uri_text)
+	# print 'Full'
+	# print expanded_uri_search.group()
+	# print 'Domain'
 	
 	if expanded_uri_search is None:
 		return None
 	else:
-		print expanded_uri_search.groups()
-		# per the domain structure listed in the problem description, this return reconstructs
-		# the uri proper format.
-		# E.g.,  'http://boss.blogs.nytimes.com/2014/' becomes 'nytimes.com'
-		return expanded_uri_search.groups[-2] + '.' + expanded_uri_search.groups[-1]
-
-	# return urlunshort.is_shortened(uri_text.group())
-	# return unshorten_uri(uri_text.group())
-	
-# for status in test_search:
-# 	print "Tweet:"
-# 	print status.text
-# 	print uri_tweet_identifier(http_regex, status.text)
-# 	print ''
-
-# print 'Basic check on is_shortened:'
-# print urlunshort.is_shortened("http://bit.ly/qlKaI")
+		# print expanded_uri_search.group(1)
+		return expanded_uri_search.group(1)	
 
 # Use tweety's api date search functionality to create a dict mapping handle to tweets
 
@@ -139,12 +111,9 @@ for handle in handle_list:
 
 response_list = [{handle: {handle2: {} for handle2 in handle_list if handle2 != handle} for handle in handle_list}]
 
+# Compares domain counts between handles for a given domain, and adds to response_list for each handle
+# if the counts are both above the given minimum
 # Should be optimized to only compare once
-
-# print domain_count_dict
-# print ''
-# print ''
-# print response_list
 
 for handle in handle_list:
 	for handle2 in response_list[0][handle].keys():
@@ -154,13 +123,12 @@ for handle in handle_list:
 					response_list[0][handle][handle2][domain] = True 
 					response_list[0][handle2][handle][domain] = True 
 
+# Removes blank values from key:value pairs
 
-# print 'Domain Count Array'
-# print domain_count_dict
+json_response = response_list[0].copy()
+for handle in response_list[0].keys():
+	json_response[handle] = {k:v for k, v in response_list[0][handle].iteritems() if v}
 
-# print 'Response Array'
-# print response_list[0]['DanielMorain']
+json_return = json.dumps([json_response], separators=(',',':'))
 
-print ''
-print ''
-print response_list
+print json_return
